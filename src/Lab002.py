e4ecc72b87a004b1ec280a9a6f68cde16a055374
Dell <amul.allu55@gmail.com>
2025-10-15 08:43:40 +0530

    git commit

---
+++ src/Lab002.py
@@ -0,0 +1,17 @@
+a = 3
+b = 9
+c = b
+
+
+print(c)
+
+x, y, z = 12, 100023.34, "amul"
+
+print(z)
+print(type(y)) # type is used to know which data type the variable is
+
+# format is used to print 2 diff data types, to declare {}{}.format
+
+print("{}{}".format( "my salary is ", y))
+
+
