a35067be334277a23e5762ecb8c7380d0cfa7996
Dell <amul.allu55@gmail.com>
2025-10-15 08:44:53 +0530

    git commit

---
+++ src/Lab001_print.py
@@ -0,0 +1,27 @@
+r= float(input("Enter your input"))
+pi = 3.14
+
+area = pi * r * 2
+# string data formating
+print(f"The area of the circle with radius {r} is {area:.2f} square units.")
+
+# f is the format
+
+print("----")
+
+base = float(input("Enter base number"))
+height = float(input("Enter height"))
+
+area = 0.5 * base * height
+
+print("the area of the triangle is", area)
+
+print("---------------------------------------------------")
+
+# operators
+
+a = int(input("Enter a number"))
+b = int(input("Enter b number"))
+
+res = a + b
+print(res)
